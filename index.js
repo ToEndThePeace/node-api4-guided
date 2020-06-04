11e4d698f7b96976d11892c5baad06d01f9835f4
unknown <bran.ramirez.don@gmail.com>
2020-06-04 10:55:05 -0700

    small change

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ require("dotenv").config();
 
 const server = require("./api/server.js");
 
-// const process = {
-//   env: {
-//     // The computer the server is running on
-//     PORT: 23476,
-//   },
-// };
-
 const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
